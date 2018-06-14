e9bea682a23164db6c8f77dc83c962faa25437c7
Elvis Quilapi <elvisquilapi@gmail.com>
2018-06-13 20:12:00 -0400

    no message

---
+++ README.md
@@ -0,0 +1,16 @@
+# Vue Webpack Bootstrap V3 Boilerplate
+
+> Proyecto hecho en Vue JS
+
+## Build Setup
+
+```bash
+# Instalar dependecias antes de comenzar
+npm install
+
+# Levantar el proyecto el localhost:8080
+npm run dev
+
+# Compilar el proyecto para producción
+npm run build
+```
---
+++ src/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Vue Webpack & Bootstrap v3.3.7 Boilerplate.'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
